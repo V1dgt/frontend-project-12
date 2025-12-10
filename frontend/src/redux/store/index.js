0fe66b5dc2a8db1de7992b0e0a74e6b590b46b4f
v1dgt <v1dgt@mail.ru>
2025-12-10 19:21:23 +0300

    add redux

---
+++ frontend/src/redux/store/index.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { channelsApi } from './channelsApi.js';
+import { messagesApi } from './messagesApi.js';
+import ui from './uiSlice.js';
+import authSlice from './authSlice.js';
+
+const createStore = () => configureStore({
+  reducer: {
+    ui,
+    auth: authSlice,
+    [channelsApi.reducerPath]: channelsApi.reducer,
+    [messagesApi.reducerPath]: messagesApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(channelsApi.middleware).concat(messagesApi.middleware),
+});
+
+export default createStore;
